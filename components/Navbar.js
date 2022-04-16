d2c6edd8f4da773b484eafaed16240850d65b719
Mark Winters <nulimitz360@gmail.com>
2022-04-16 11:08:21 -0700

    removed .env file

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <div className="container">
+        <div className="container-fluid">
           {session && (
             <button
               className="navbar-toggler"
