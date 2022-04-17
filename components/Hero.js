6a0eb9935556478ca44057723dd1872ac9997fed
Mark Winters <nulimitz360@gmail.com>
2022-04-16 18:36:23 -0700

    Added sidebar

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
-            <img
+            <Image
               src="/images/world_map.png"
               className="d-block mx-lg-auto img-fluid"
               alt="NextJS Auth"
