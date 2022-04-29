7d693ed501718cbb5d5c03bdbfe32e8fc3ebe80b
Mark Winters <nulimitz360@gmail.com>
2022-04-29 09:14:28 -0600

    Fixed user role and dashboard

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,8 @@ export default function Index() {
   });
 
   return (
-    <Layout title="Dashboard">
-      <div className="container mt-3 pt-3">
+    <Layout title="Dashboard" sidebar>
+      <div className="container-fluid mt-3 pt-3">
         <h1>Dashboard</h1>
       </div>
     </Layout>
