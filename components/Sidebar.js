16df30a04db4596fabb7e0d01b2bf04fc0bf8357
Mark Winters <nulimitz360@gmail.com>
2022-04-29 13:02:05 -0600

    Added links to sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 const Sidebar = () => {
   const router = useRouter();
+  const { data: session } = useSession();
 
   return (
     <div
@@ -19,76 +20,153 @@ const Sidebar = () => {
                 Dashboard
               </div>
             </li>
-            <li>
-              <Link href="/" passHref>
-                <a
-                  className={`nav-link px-3 ${
-                    router.pathname == "/" ? `active` : ""
-                  }`}
-                >
-                  <span className="me-2">
-                    <i className="bi bi-house"></i>
-                  </span>
-                  <span>Home</span>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#" passHref>
-                <a
-                  className={`nav-link px-3 ${
-                    router.pathname == "#" ? `active` : ""
-                  }`}
-                >
-                  <span className="me-2">
-                    <i className="bi bi-link"></i>
-                  </span>
-                  <span>Link</span>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#" passHref>
-                <a
-                  className={`nav-link px-3 ${
-                    router.pathname == "#" ? `active` : ""
-                  }`}
-                >
-                  <span className="me-2">
-                    <i className="bi bi-link"></i>
-                  </span>
-                  <span>Link</span>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#" passHref>
-                <a
-                  className={`nav-link px-3 ${
-                    router.pathname == "#" ? `active` : ""
-                  }`}
-                >
-                  <span className="me-2">
-                    <i className="bi bi-link"></i>
-                  </span>
-                  <span>Link</span>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#" passHref>
-                <a
-                  className={`nav-link px-3 ${
-                    router.pathname == "#" ? `active` : ""
-                  }`}
-                >
-                  <span className="me-2">
-                    <i className="bi bi-link"></i>
-                  </span>
-                  <span>Link</span>
-                </a>
-              </Link>
-            </li>
+            {session.user.role === "admin" ? (
+              <>
+                <li>
+                  <Link href="/" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "/" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-house"></i>
+                      </span>
+                      <span>Home</span>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "#" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-link"></i>
+                      </span>
+                      <span>Link</span>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "#" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-link"></i>
+                      </span>
+                      <span>Link</span>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "#" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-link"></i>
+                      </span>
+                      <span>Link</span>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "#" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-link"></i>
+                      </span>
+                      <span>Link</span>
+                    </a>
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link href="/" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "/" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-house"></i>
+                      </span>
+                      <span>Home</span>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "#" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-link"></i>
+                      </span>
+                      <span>Link</span>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "#" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-link"></i>
+                      </span>
+                      <span>Link</span>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "#" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-link"></i>
+                      </span>
+                      <span>Link</span>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" passHref>
+                    <a
+                      className={`nav-link px-3 ${
+                        router.pathname == "#" ? `active` : ""
+                      }`}
+                    >
+                      <span className="me-2">
+                        <i className="bi bi-link"></i>
+                      </span>
+                      <span>Link</span>
+                    </a>
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
           <ul className="navbar-nav fixed-bottom py-3">
             <li>
