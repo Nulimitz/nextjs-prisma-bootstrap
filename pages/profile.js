5097daf40f6296960b789abc84decc89edc367a5
Mark Winters <nulimitz360@gmail.com>
2022-05-15 08:42:44 -0600

    Fixed profile page

--- pages/profile.js
+++ pages/profile.js
@@ -63,13 +63,13 @@ export default function Profile({
                 </div>
                 <div className="col-sm-9 text-secondary">{email}</div>
               </div>
+              <hr />
               <div className="row">
                 <div className="col-sm-3">
                   <h6 className="mb-0">Role</h6>
                 </div>
                 <div className="col-sm-9 text-secondary">{role}</div>
               </div>
-              <hr />
               {accounts.map((accounts, i) => (
                 <>
                   <hr key={accounts.id} />
